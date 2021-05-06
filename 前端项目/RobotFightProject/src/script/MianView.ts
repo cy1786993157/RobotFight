34b15b4cb45a78664de0809717d44b0cdbed4cf8
chenyong <1786993157@qq.com>
2021-05-07 00:06:52 +0800

    Laya出版提交

---
+++ 前端项目/RobotFightProject/src/script/MianView.ts
@@ -0,0 +1,21 @@
+import { ui } from "../ui/layaMaxUI";
+
+export default class MainView extends ui.scene.MianViewUI
+{
+
+   constructor()
+   {
+       super();
+   }
+
+   onEnable()
+   {
+
+   }
+
+   onDisable()
+   {
+
+   }
+
+}
